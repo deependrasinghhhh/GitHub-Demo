a4d8daa4e6bfd57dbd61927178209f39cc2fcfa4
CCST <deependradev306@gmail.com>
2026-09-08 12:05:15 +0530

    feat: add palindrome check functionality in deep.js

--- deep.js
+++ deep.js
@@ -1,1 +1,4 @@
-console.log(`Hello, World!`);
+console.log(`Hello, World!`);      // create a palindrome string
+let str = "racecar";
+let isPalindrome = str === str.split("").reverse().join("");
+console.log(`Is "${str}" a palindrome? ${isPalindrome}`);
